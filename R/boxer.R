f613615c9e1445f75d1c1df6f894314a7b8a173c
stomperusa <stomperusa@verizon.net>
2020-03-15 20:42:11 -0400

    Second iteration with boxer function documented, no errors, warnings, or notes.

--- R/boxer.R
+++ R/boxer.R
@@ -1,5 +1,51 @@
+#' Create a bounding box in an image based on selected points
+#'
+#' \code{boxer} allows the user to identify an area in an image around which to
+#' generate a bounding box.
+#'
+#' Stream a series of JPEG or PNG images from a directory that each appears on
+#' the screen, and indicate between 2 and 26 points that define an area around
+#' which a bounding box is generated based on the max/min x and y values of the
+#' selected coordinates. There are two outputs. One is a dataframe with the name
+#' of the original image, the coordinates of the bounding box ("x_left",
+#' "y_top", "x_right", "y_bottom") and the classifier. Another is, for each
+#' image, an image file "out_<original_file_name>" annotated with the bounding
+#' box, the classifier printed above the bounding box if show_classifer is set
+#' to TRUE, and resized if either the size_x or size_y parameter is given.
+#'
+#'
+#'
+#' @param names A list that contains the names of the image files to cycle
+#'   through.
+#' @param file_path_input The directory that contains the image files in JPEG
+#'   format.
+#' @param file_path_output The directory to which the annotated images should be
+#'   saved.
+#' @param color The color of the bounding box. Default is "red".
+#' @param size_x Number of columns to resize the x-axis. Default is NA and will
+#'   return size of original image.
+#' @param size_y Number of columns to resize the y-axis. Default is NA and will
+#'   return size of original image.
+#' @param classifier Character string to add a classifier along with the output
+#'   coordinates. Default is NA.
+#' @param show_classifier Logical to indicate to print the classifier along with
+#'   the bounding box. Default is F.
+#'
+#' @return A dataframe with the bounding box coordinates for each image, and a
+#'   new image file with the bounding box annotation for each of the images
+#'   processed.
+#'
+#' @examples
+#' \dontrun{
+#' boxer(names = c("dog.275.jpg", "dog.544.jpg", "dog.119.jpg"),
+#' file_path_input = "/dog_pics/input/", file_path_output = "/dog_pics/output/",
+#' color = "blue", size_x = 224, size_y = 224, classifier = "dog",
+#' show_classifier = T)
+#' }
+#'
+#'@export
 boxer <- function(names, file_path_input, file_path_output, color = "red",
-                  size_x = NA, size_y = NA, classifier = NA, show_classifier = F, ...){
+                  size_x = NA, size_y = NA, classifier = NA, show_classifier = F){
 
     #Empty data frame used to capture bounding box coorindates
     points_master <- data.frame()
